65f940a0f67c789b9d76d3ef6023fba48638ca5b
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2020-11-23 13:53:46 -0300

    Criação dos controllers e inserção do index.php fora da pasta src

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    
+    require_once 'src/core/Core.php';
+
+    require_once 'src/config.php';
+
+    require_once 'src/controller/HomeController.php';
+    require_once 'src/controller/ErroController.php';
+    require_once 'src/model/medico.php';
+
+    $template = file_get_contents('src/template/estrutura.html');
+
+    ob_start();
+        $core = new Core;
+        $core->start($_GET);
+
+        $saida = ob_get_contents();
+    ob_end_clean();
+
+
+    $template_pronto = str_replace('{{area_dinamica}}', $saida, $template);
+    echo $template_pronto;
+?>
+
+    <!-- if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/modelo-fc/src/view/');
+    exit; -->
