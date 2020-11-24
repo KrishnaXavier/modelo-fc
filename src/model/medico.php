538f72b8168a3f6b3a8b58c7cae22f95c023735b
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2020-11-24 18:56:58 -0300

    alteração select e verificação  da variavel

--- src/model/medico.php
+++ src/model/medico.php
@@ -2,6 +2,28 @@
 
     class medico{
         //lembre que static acessa o método sem precisar criar instância da classe
+        public static function selectLista()
+        {
+            $con = Connection::getCon();
+
+            $sql = "Select m.id, m.nome, m.email, m.data_criacao, h.id as id_consulta, h.horario_agendado from medico m, horario h WHERE m.id = h.id_medico order by m.id";
+            $sql = $con->prepare($sql);
+            $sql -> execute();
+
+            $resultado = array();
+
+            while ($row = $sql->fetchObject('medico')){
+                $resultado[] = $row;
+            }
+            if(!$resultado){
+                throw new Exception("Sem Registros");
+            }
+            
+            return $resultado;
+
+        }
+
+
         public static function selectAll()
         {
             $con = Connection::getCon();
@@ -10,23 +32,22 @@
             $sql = $con->prepare($sql);
             $sql -> execute();
 
-            // var_dump($sql->fetchALL);
-            
             while ($row = $sql->fetchObject('medico')){
                 $resultado[] = $row;
-
             }
             if(!$resultado){
                 throw new Exception("Sem Registros");
             }
+            // var_dump($resultado);
             return $resultado;
+
         }
 
         public static function selectById($idPost)
         {
             $con = Connection::getCon();
             
-            $sql = "Select * from medico where id = :id";
+            $sql = "Select m.id, m.nome, m.email, h.id, h.horario_agendado from medico m, horario h where m.id = :id";
             $sql = $con->prepare($sql);
 			$sql->bindValue(':id', $idPost, PDO::PARAM_INT);
             $sql -> execute();
@@ -39,6 +60,27 @@
             return $resultado;
         }
         public static function insert($dadosForm)
+        {
+            var_dump($dadosForm);
+            if(empty($dadosForm['email']||empty($dadosForm['nome'])||empty($dadosForm['senha'])))
+            {
+                throw new Exception("Faltando Dados");
+                return false;
+            }
+            $con = Connection::getCon();
+
+            $sql = 'Insert into medico (email, nome, senha) values (:email, :nome, :senha)';
+            
+            $sql = $con->prepare($sql);
+			$sql->bindValue(':email', $dadosForm['email']);
+			$sql->bindValue(':nome', $dadosForm['nome']);
+			$sql->bindValue(':senha', $dadosForm['senha']);
+            $sql -> execute();
+
+            // var_dump($sql);
+
+        }
+        public static function alter($dadosForm)
         {
             var_dump($dadosForm);
             if(empty($dadosForm['email']||empty($dadosForm['nome'])||empty($dadosForm['senha'])))
